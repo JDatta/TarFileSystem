95c9f4de40e11365b80bd9a3df73555c99b410ea
Joydip Datta <jdatta@talena-inc.com>
2017-01-30 14:50:12 +0530

    Add more unit tests
    Added more unit tests to test subdirectories within a tar archive

--- src/test/java/org/apache/hadoop/fs/tar/TestTarFileSystem.java
+++ src/test/java/org/apache/hadoop/fs/tar/TestTarFileSystem.java
@@ -3,26 +3,65 @@ package org.apache.hadoop.fs.tar;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.net.URISyntaxException;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.tar.test.TarFileSystemTestFramework;
 import org.apache.hadoop.fs.tar.test.TestUtils;
 import org.junit.Test;
 
+import junit.framework.Assert;
 
 public class TestTarFileSystem extends TarFileSystemTestFramework {
 
+  private static final String SAMPLE_TEXT =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+      + "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+      + "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris "
+      + "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in "
+      + "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
+      + "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
+      + "culpa qui officia deserunt mollit anim id est laborum.";
+
   @Override
   protected void createTarFile() throws IOException {
-    TestUtils.createLocalTarFile(getTestTarFile(), 10);
+    TestUtils.createLocalTarFile(
+      this.getTestTarFile(), "", SAMPLE_TEXT, 10);
   }
 
   @Test
   public void testListStatus() throws IOException, URISyntaxException {
-
-    FileStatus[] stuses = getTarfs().listStatus(getTestTarPath());
-    assertEquals(stuses.length, 10);
+    assertEquals(this.getTarfs().listStatus(this.getTestTarPath()).length, 10);
   }
 
+  @Test
+  public void testGetFileStatus() throws IOException, URISyntaxException {
+    final FileStatus[] stats = this.getTarfs().listStatus(this.getTestTarPath());
+    assertEquals(stats.length, 10);
+    for (int i = 0; i < stats.length; i++) {
+      Assert.assertEquals(
+        stats[i], this.getTarfs().getFileStatus(stats[i].getPath()));
+    }
+  }
+
+  @Test
+  public void testRead() throws IOException, URISyntaxException {
+    final FileStatus[] stats = this.getTarfs().listStatus(this.getTestTarPath());
+    assertEquals(stats.length, 10);
+    for (int i = 0; i < stats.length; i++) {
+      InputStream in = null;
+      try {
+        System.out.println(stats[i].getPath());
+        in = this.getTarfs().open(stats[i].getPath());
+        final StringWriter writer = new StringWriter();
+        IOUtils.copy(in, writer);
+        Assert.assertEquals(SAMPLE_TEXT + i, writer.toString());
+      } finally {
+        IOUtils.closeQuietly(in);
+      }
+    }
+  }
 }
--- src/test/java/org/apache/hadoop/fs/tar/test/TestUtils.java
+++ src/test/java/org/apache/hadoop/fs/tar/test/TestUtils.java
@@ -7,17 +7,18 @@ import java.io.OutputStream;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.hadoop.fs.Path;
 
 public class TestUtils {
 
   public static void createLocalTarFile(File tarFile, int count)
       throws IOException {
 
-    createLocalTarFile(tarFile, "file_", count);
+    createLocalTarFile(tarFile, "files", "Lorem", count);
   }
 
   public static void createLocalTarFile(
-      File tarFile, String filePrefix, int count)
+    File tarFile, String prefix, String message, int count)
           throws IOException {
 
     TarArchiveOutputStream tarOutput = null;
@@ -26,9 +27,22 @@ public class TestUtils {
       tarOutput = new TarArchiveOutputStream(os);
 
       for (int i = 0; i < count; i++) {
-        String msg = "Lorem";
-        byte[] bytes = msg.getBytes();
-        TarArchiveEntry entry = new TarArchiveEntry(filePrefix + i);
+        String thisMessage = message + i;
+        byte[] bytes = thisMessage.getBytes();
+        // put the i-th file in i-th level directory
+        // i.e. 2nd file is placed in prefix/dir1/dir2/file
+        // 3rd file is placed in prefix/dir1/dir2/dir3/file and so on
+        StringBuilder thisPrefix = new StringBuilder(prefix);
+        for (int j = 0; j < i; j++) {
+          thisPrefix.append(Path.SEPARATOR);
+          thisPrefix.append("dir");
+          thisPrefix.append(j);
+        }
+        thisPrefix.append(Path.SEPARATOR);
+        thisPrefix.append("file_");
+        thisPrefix.append(i);
+
+        TarArchiveEntry entry = new TarArchiveEntry(thisPrefix.toString());
         entry.setSize(bytes.length);
         tarOutput.putArchiveEntry(entry);
         tarOutput.write(bytes);
